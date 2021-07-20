0b9be982636b477a78418bd7dbd1fd4721840005
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-07-20 15:42:33 -0700

    Update Database.py

--- Database/Database.py
+++ Database/Database.py
@@ -7,9 +7,9 @@ conn = sqlite3.connect('assignment.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_cities(\
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_for_database_assignment(\
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_city_name TEXT \
+        col_random_string TEXT \
         )")
     conn.commit()
 conn.close()
@@ -21,15 +21,24 @@ conn = sqlite3.connect('test.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_cities(col_city_name) VALUES (?)", \
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
                 ('New York City'))
-    cur.execute("INSERT INTO tbl_cities(col_city_name) VALUES (?)", \
-                ('Los Angeles'))
-    cur.execute("INSERT INTO tbl_cities(col_city_name) VALUES (?)", \
-                ('Chicago'))
-    cur.execute("INSERT INTO tbl_cities(col_city_name) VALUES (?)", \
-                ('Houston'))
-    cur.execute("INSERT INTO tbl_cities(col_city_name) VALUES (?)", \
-                ('Phoenix'))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
+                ('Basketball'))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
+                ('Fence'))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
+                ('Street'))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
+                ('Sign'))
     conn.commit()
 conn.close()
+
+
+
+# return all files as a list
+for file in os.listdir(r'D:\GFG'):
+     # check the files which are end with specific extention
+    if file.endswith(".png"):
+        # print path name of selected files
+        print(os.path.join(r'C:\GFG\Screenshots', file))
