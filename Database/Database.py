399856cdc5c1b172368998943ba68057a815eeba
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-07-21 23:19:28 -0700

    Update Database.py

--- Database/Database.py
+++ Database/Database.py
@@ -1,44 +1,43 @@
 
 import sqlite3
 
-
-conn = sqlite3.connect('assignment.db')
-
+# Creating database
+conn = sqlite3.connect('db_assignment.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_for_database_assignment(\
+    
+    # Creating table
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_for_database_assignment( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_random_string TEXT \
+        col_file_list TEXT \
         )")
+    
+    # Inserting values into table
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
+                ('information.docx',))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
+                ('Hello.txt',))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
+                ('myImage.png',))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
+                ('myMovie.mpg',))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
+                ('World.txt',))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
+                ('data.pdf',))
+    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
+                ('myPhoto.jpg',))
+    #Printing file names ending in '.txt'
+    
+    cur.execute("SELECT col_file_list FROM tbl_for_database_assignment WHERE col_file_list LIKE '%.txt'")
+    row = cur.fetchall()
+    print(row)
     conn.commit()
 conn.close()
 
 
 
 
-conn = sqlite3.connect('test.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
-                ('New York City'))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
-                ('Basketball'))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
-                ('Fence'))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
-                ('Street'))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_random_string) VALUES (?)", \
-                ('Sign'))
-    conn.commit()
-conn.close()
 
 
-
-# return all files as a list
-for file in os.listdir(r'D:\GFG'):
-     # check the files which are end with specific extention
-    if file.endswith(".txt"):
-        # print path name of selected files
-        print(os.path.join(r'C:\GFG\Screenshots', file))
