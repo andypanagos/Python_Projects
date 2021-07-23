aeea90da01cd06f563638a4cc7c718b3471216a6
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-07-22 17:27:21 -0700

    Update Database.py

--- Database/Database.py
+++ Database/Database.py
@@ -1,43 +1,36 @@
 
 import sqlite3
 
-# Creating database
-conn = sqlite3.connect('db_assignment.db')
 
-with conn:
-    cur = conn.cursor()
-    
+def Create_Table():
+    # Creating database
+    conn = sqlite3.connect('assignment.db')
+    with conn:
+        cur = conn.cursor()
     # Creating table
-    cur.execute("CREATE TABLE IF NOT EXISTS tbl_for_database_assignment( \
-        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_file_list TEXT \
-        )")
-    
-    # Inserting values into table
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
-                ('information.docx',))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
-                ('Hello.txt',))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
-                ('myImage.png',))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
-                ('myMovie.mpg',))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
-                ('World.txt',))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
-                ('data.pdf',))
-    cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", \
-                ('myPhoto.jpg',))
-
-    #Printing file names ending in '.txt'
-    cur.execute("SELECT col_file_list FROM tbl_for_database_assignment WHERE col_file_list LIKE '%.txt'")
-    row = cur.fetchall()
-    print(row)
-    conn.commit()
-conn.close()
+        cur.execute("CREATE TABLE IF NOT EXISTS tbl_for_database_assignment( \
+                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                col_file_list TEXT \
+                )")
+        conn.commit()
+    conn.close()
 
 
+def File_List():
+    # File names to look through
+    fileList = ('information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+    for var1 in fileList:
+        # Finding files ending with ".txt"
+        if var1.endswith('.txt'):
+            conn = sqlite3.connect('assignment.db')
+            # Inserting file names into table in database
+            with conn:
+                cur = conn.cursor()
+                cur.execute("INSERT INTO tbl_for_database_assignment(col_file_list) VALUES (?)", (var1,))
+                conn.commit()
+            conn.close()
 
 
-
-
+if __name__ == "__main__":
+    Create_Table()
+    File_List()
