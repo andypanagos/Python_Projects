2cde3535a7622fe7d74a9bc9ff477156ad4745c1
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-07-22 18:14:34 -0700

    Update Polymorphism.py

--- Polymorphism/Polymorphism.py
+++ Polymorphism/Polymorphism.py
@@ -5,26 +5,19 @@ class Motor_Vehicle:
     make = "Honda"
     year = "2019"
     color = "black"
-
+    # Function gets vehicle info
     def getVehicleInfo(self):
         print("The vehicles here are all made by {} in {}, and are all {}.".format(self.make, self.year, self.color))
          
-
-
-
-
 
 # 1st Child Class "Car"
 class Car(Motor_Vehicle):
     model = "Civic"
     wheels = 4
     seats = 5
-
+    # Function gets vehicle info
     def getVehicleInfo(self):
         print("The car is a {} model, so obviously it has {} wheels and {} seats.".format(self.model, self.wheels, self.seats))
-        
-    
-
 
 
 # 2nd Child Class "Motorcycle"
@@ -33,8 +26,7 @@ class Motorcycle(Motor_Vehicle):
     wheels = 2
     seats = 1
     handlebars = "Ape Hanger"
-
-
+    # Function gets vehicle info
     def getVehicleInfo(self):
         print("The motorcycle is a {} model. Comes with {} handlebars.".format(self.model, self.handlebars))
 
