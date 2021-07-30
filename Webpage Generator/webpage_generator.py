64253cd9e0940c0222116f70ef55cc7ca537eeaa
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-07-30 00:19:38 -0700

    Create webpage_generator.py

---
+++ Webpage Generator/webpage_generator.py
@@ -0,0 +1,19 @@
+
+
+# Create new html file in write mode
+f = open('webpage_generator.html', 'w')
+
+# HTML code that will go into "webpage_generator.html"
+html_template = """<html>
+    <body>
+    <h1>Stay tuned for our amazing summer sale!</h1>
+    </body>
+    </html>
+    """
+
+
+# Writing code into file
+f.write(html_template)
+
+# Close the file
+f.close()
