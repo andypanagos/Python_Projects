9059e3b2155ca29eb11bfcb3be334c0193afe4b9
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-03 23:07:57 -0700

    Create file_transfer.py

---
+++ File Transfer/file_transfer.py
@@ -0,0 +1,15 @@
+#
+# Python Ver:   3.9.6
+#
+# Author:       Jeff Stoppel
+#
+# Purpose:      To create a script that will transfer files
+#               
+#
+
+
+import shutil
+import os
+
+# Set where the source of teh files are
+source = '
