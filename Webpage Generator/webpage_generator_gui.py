9c9278fc0bdc44c94d60df66e0dff20df6d96872
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-04 22:05:47 -0700

    Create webpage_generator_gui.py

---
+++ Webpage Generator/webpage_generator_gui.py
@@ -0,0 +1,84 @@
+#
+# Python Ver:   3.9.6
+#
+# Author:       Jeff Stoppel
+#
+# Purpose:      To create a GUI that takes a user's input
+#               and sets it as the body text for a web page
+#
+#
+#
+#
+
+
+import tkinter
+from tkinter import *
+import webbrowser
+
+import gui_func
+
+
+
+class ParentWindow(Frame):
+    def __init__(self, master):
+        Frame. __init__ (self)
+
+
+        # Creating master frame
+        self.master = master
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('{}x{}'.format(500, 300))
+        gui_func.center_window(self,500,300)
+        self.master.title('Set Text')
+        self.master.config(bg='lightgray')
+
+
+        # Creating text input box
+        self.varBodyText = StringVar()
+
+        self.lblBodyText = Label(self.master, text='Enter text: ', font=("Helvetica", 16), fg='black', bg='lightgray')
+        self.lblBodyText.grid(row=0, column=0, padx=(20,0), pady=(30,0))
+
+        self.txtBodyText = Entry(self.master,text=self.varBodyText, font=("Helvetica", 16), fg='black', bg='white')
+        self.txtBodyText.grid(row=0, column=1, padx=(15,0), pady=(30,0))
+        
+
+        # Creating Buttons
+        self.btnSubmit = Button(self.master, text="Submit", width=10, height=2, command=self.submit)
+        self.btnSubmit.grid(row=2, padx=(0,0), pady=(15,0))
+
+        self.btnCancel = Button(self.master, text="Cancel", width=10, height=2, command=self.cancel)
+        self.btnCancel.grid(row=2, column=1, padx=(15,0), pady=(15,0), sticky=W)
+        
+                             
+    # Defining button functions
+    def submit(self):
+        bt = self.txtBodyText.get()     # User input
+        f = open('new_webpage.html', 'w')   
+        new_template = """<html>
+        <body>
+        <h1>{}</h1>
+        </body>
+        </html>
+        """.format(bt)      # Inserting user input into html file
+        f.write(new_template)
+        f.close()
+        webbrowser.open_new_tab('new_webpage.html')
+
+
+    def cancel(self):
+        self.master.destroy()
+            
+            
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
