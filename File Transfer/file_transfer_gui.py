5c6e7573afa46b0c4bccde2e050b1164675431b8
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-06 01:13:13 -0700

    Update file_transfer_gui.py

--- File Transfer/file_transfer_gui.py
+++ File Transfer/file_transfer_gui.py
@@ -30,42 +30,80 @@ class ParentWindow(Frame):
         self.master = master
         self.master.resizable(width=False, height=False)
         self.master.geometry('{}x{}'.format(500, 250))
-        # Center window function from another file here
+        center_window(self,500,250)
         self.master.title('File Transfer')
-        
 
-       
-
-        
-
-        # Creating Source Folder text field
+        # Creating Source Folder Label
         self.lblSource = Label(self.master, text='Source Folder Path:')
         self.lblSource.pack(pady=5)
+        
+        # Source File Path
         self.txtSource = Entry(self.master, text='', width=50)
         self.txtSource.pack(pady=5)
-        self.btBrowse = Button(self.master, text='Browse')
+        
+        # Browse button
+        self.btBrowse = Button(self.master, text='Browse', command=source)
         self.btBrowse.pack(pady=5)
 
-
+        # Line to seperate source section from destination section
         self.line = Canvas(self.master, height=1, width=500, bg='grey', relief='groove')
         self.line.pack(pady=5)
             
-
+        # Creating Destination Folder Label
         self.lblDestination = Label(self.master, text='Destination Folder Path:')
         self.lblDestination.pack(pady=5)
+        # Destination File Path
         self.txtDestination = Entry(self.master, text='', width=50)
         self.txtDestination.pack(pady=5)
-        self.btBrowse2 = Button(self.master, text='Browse')
+        # Browse button
+        self.btBrowse2 = Button(self.master, text='Browse', command=destination)
         self.btBrowse2.pack(pady=5)
 
-        
-        self.btCopyButton = Button(self.master, text='Copy Files to Destination', width=50)
+        # Button to copy files from source folder to destination folder
+        self.btCopyButton = Button(self.master, text='Copy Files to Destination', width=50, command=copy_files)
         self.btCopyButton.pack(pady=5)
 
 
+# Method to copy modified files from one folder to another        
+def copy_files():
+    source = self.txtSource.get()
+    destination = self.txtDestination.get()
+    for fname in os.listdir(source):
+        src_fname = os.path.join(source, fname)
+        if last_mod_time(src_fname) > before:
+            destination_fname = os.path.join(destination, fname)
+            shutil.copy(source+fname, destination)
+
+# Defining method to center GUI
+def center_window(self, w, h):
+    screen_width = self.master.winfo_screenwidth()
+    screen_height = self.master.winfo_screenheight()
+    x = int((screen_width/2) - (w/2))
+    y = int((screen_height/2) - (h/2))
+    centerGeo = self.master.geometry('{}x{}+{}+{}'.format(w, h, x, y))
+    return centerGeo
+    
+
+# Function to browse and select source folder
+def source():
+    self.lblSource = tk.filedialog.askdirectory()
+    self.txtSource.delete(1, tk.END)
+    self.txtSource.insert(0, self.lblSource)
+
+# Function to browse and select destination folder
+def destination():
+    self.lblDestination = tk.filedialog.askdirectory()
+    self.txtDestination.delete(1, tk.END)
+    self.txtDestination.insert(0, self.lblDestination)
 
 
+# Time Frame
+SECONDS_IN_DAY = 24 * 60 * 60
+now = time.time()
+before = now - SECONDS_IN_DAY
 
+def last_mod_time(fname):
+    return os.path.getmtime(fname)
 
 
 
