1be271bc25c5cd9a699ddd738e22526735979363
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-12 22:27:50 -0700

    Added DjangoClasses and registered it

---
+++ DjangoUniversity/Classes/classApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class DjangoClasses(models.Model):
+    Title = models.CharField(max_length=50)
+    Course_Number = models.IntegerField()
+    Instructor_Name = models.CharField(max_length=50)
+    Duration = models.FloatField()
