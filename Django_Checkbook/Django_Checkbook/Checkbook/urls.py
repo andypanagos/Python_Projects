f99ca699b241ec7bf2b8f4b6385dbc8a90937ea0
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-19 15:17:57 -0700

    Added spacing between lines

--- Django_Checkbook/Django_Checkbook/Checkbook/urls.py
+++ Django_Checkbook/Django_Checkbook/Checkbook/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='index'),
     path('create/', views.create_account, name='create'),
     path('balance/', views.balance, name='balance'),
-    paht('transaction/', views.transaction, name='transaction')
+    path('transaction/', views.transaction, name='transaction')
 ]
