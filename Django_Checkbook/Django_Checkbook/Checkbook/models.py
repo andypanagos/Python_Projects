41d3705ae0510edbf830ad1eea7c1bbb53d4a5d7
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-19 17:10:54 -0700

    Added class 'Account' to models.py

--- Django_Checkbook/Django_Checkbook/Checkbook/models.py
+++ Django_Checkbook/Django_Checkbook/Checkbook/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Account(model.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    initial_deposit = models.DecimalField(max_digits=15, decimal_places=2)
+
+    Accounts = models.Manager()
+
+    # Allows references to a specific account be returned
+    # as the owner's name not the primary key
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
