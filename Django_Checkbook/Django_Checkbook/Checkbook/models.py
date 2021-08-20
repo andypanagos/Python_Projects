cbd9e45d5c1e15022ba6a47f8767874563f00655
jeffstop <84537804+jeffstop@users.noreply.github.com>
2021-08-19 17:21:43 -0700

    Added 'Transaction' to models.py

--- Django_Checkbook/Django_Checkbook/Checkbook/models.py
+++ Django_Checkbook/Django_Checkbook/Checkbook/models.py
@@ -12,3 +12,17 @@ class Account(model.Model):
     # as the owner's name not the primary key
     def __str__(self):
         return self.first_name + ' ' + self.last_name
+
+
+TransactionTypes = [('Deposit', 'Deposit'), ('Withdrawal', 'Withdrawal')]
+
+
+class Transaction(models.Model):
+    date = models.DateField()
+    type = models.CharField(max_length=10, choices=TransactionTypes)
+    amount = models.DecimalField(max_digits=15, decimal_places=2)
+    description = models.CharField(max_length=100)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    Transactions = models.Manager()
+
